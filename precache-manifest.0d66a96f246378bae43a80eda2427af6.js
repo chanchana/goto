1b20f365db60c129a95be07d7ac0a4c93616d865
Chanchana Wicha <chanchana@Chanchanas-MacBook-Pro.local>
2020-03-11 19:15:44 +0700

    Updates

---
+++ precache-manifest.0d66a96f246378bae43a80eda2427af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155818a1829a5e0732aee83d799783ef",
+    "url": "/goto/index.html"
+  },
+  {
+    "revision": "c8e14baa6f0e5d07131d",
+    "url": "/goto/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "828432ee8a36acfcbdef",
+    "url": "/goto/static/js/2.e547c234.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/goto/static/js/2.e547c234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e14baa6f0e5d07131d",
+    "url": "/goto/static/js/main.5cd88dbb.chunk.js"
+  },
+  {
+    "revision": "293acfaf5281c1e64fd1",
+    "url": "/goto/static/js/runtime-main.6138db15.js"
+  }
+]);
